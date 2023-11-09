c75213effb68294c7845cdc55d8ca517906e22a9
Alejandro Lembke Barrientos <aleleba@hotmail.com>
2023-11-09 22:47:10 +0000

    Returning to use organizations.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Docker Github Action Runner
 
-## Version 1.0.1
+## Version 1.0.2
--- start.sh
+++ start.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-OWNER=$OWNER
-REPO=$REPO
+ORGANIZATION=$ORGANIZATION
 ACCESS_TOKEN=$ACCESS_TOKEN
 
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${OWNER}/${REPO}/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${OWNER}/${REPO} --token ${REG_TOKEN}
+./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
 
 cleanup() {
     echo "Removing runner..."
